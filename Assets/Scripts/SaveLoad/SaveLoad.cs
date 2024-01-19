c85f06aefb64292f4cd22240b77f141366c786fe
Yusuf Yilmaz <95490228+yu5ufyilmaz@users.noreply.github.com>
2024-01-19 19:18:23 +0300

    Save System Started

---
+++ Assets/Scripts/SaveLoad/SaveLoad.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.IO;
+using System.Net;
+
+public static class SaveLoad
+{
+    public static UnityAction OnSaveGame;
+    public static UnityAction<SaveData> OnLoadGame;
+
+    private static string directory = "/SaveData";
+    private static string fileName = "SaveGame.sav";
+
+    public static bool Save(SaveData data)
+    {
+        OnSaveGame?.Invoke();
+
+        string dir = Application.persistentDataPath + directory;
+
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(dir + fileName, json);
+        
+        Debug.Log("Saving game");
+
+        return true;
+    }
+
+    public static SaveData Load()
+    {
+        string fullPath = Application.persistentDataPath + directory + fileName;
+        SaveData data = new SaveData();
+
+        if (File.Exists(fullPath))
+        {
+            string json = File.ReadAllText(fullPath);
+            data = JsonUtility.FromJson<SaveData>(json);
+            
+            OnLoadGame?.Invoke(data);
+        }
+        else
+        {
+            Debug.Log("Save file doesn't exist");
+        }
+
+        return data;
+    }
+}
