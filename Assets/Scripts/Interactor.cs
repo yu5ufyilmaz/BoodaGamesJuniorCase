1ae6ee1e21a2a0ae20b1a0c3474b52cb01b702d6
Yusuf Yilmaz <95490228+yu5ufyilmaz@users.noreply.github.com>
2025-04-17 06:26:27 +0300

    Fix

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Events;
 
 public class Interactor : MonoBehaviour
 {
@@ -13,13 +12,7 @@ public class Interactor : MonoBehaviour
     
     public bool IsInteracting { get; private set; }
     
-    // Events for UI prompt
-    public Action<string> OnInteractableFound;
-    public Action OnInteractableLost;
-    
     private Camera mainCamera;
-    private IInteractable currentInteractable;
-    private bool isLookingAtInteractable = false;
 
     private void Awake()
     {
@@ -34,9 +27,7 @@ public class Interactor : MonoBehaviour
         // Debug için ışını görselleştir
         Debug.DrawRay(ray.origin, ray.direction * InteractionDistance, Color.red);
         
-        bool hitInteractable = false;
-        
-        // Işının çarptığı nesne varsa
+        // Işının çarptığı nesne varsa ve E tuşuna basılmışsa
         if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractionDistance, InteractionLayer))
         {
             // Çarpılan nesnenin IInteractable arayüzünü uygulayıp uygulamadığını kontrol et
@@ -44,34 +35,6 @@ public class Interactor : MonoBehaviour
             
             if (interactable != null)
             {
-                hitInteractable = true;
-                currentInteractable = interactable;
-                
-                // If we just started looking at an interactable
-                if (!isLookingAtInteractable)
-                {
-                    isLookingAtInteractable = true;
-                    
-                    // Get the name of the object if possible for a custom message
-                    string objectName = hitInfo.collider.gameObject.name;
-                    string displayName = "";
-                    
-                    // Try to get a better name from the object
-                    var itemPickup = hitInfo.collider.GetComponent<ItemPickUp>();
-                    if (itemPickup != null && itemPickup.ItemData != null)
-                    {
-                        displayName = $"Pick up {itemPickup.ItemData.DisplayName}";
-                    }
-                    else
-                    {
-                        // Generic interaction prompt
-                        displayName = "Interact";
-                    }
-                    
-                    // Trigger UI prompt
-                    OnInteractableFound?.Invoke(displayName);
-                }
-                
                 // E tuşuna basılmışsa etkileşime geç
                 if (Keyboard.current.eKey.wasPressedThisFrame)
                 {
@@ -79,26 +42,14 @@ public class Interactor : MonoBehaviour
                 }
             }
         }
-        
-        // If we were looking at an interactable but no longer are
-        if (isLookingAtInteractable && !hitInteractable)
-        {
-            isLookingAtInteractable = false;
-            currentInteractable = null;
-            OnInteractableLost?.Invoke();
-        }
     }
+    
+    
     
     void StartInteraction(IInteractable interactable)
     {
         interactable.Interact(this, out bool interactSuccessful);
         IsInteracting = interactSuccessful;
-        
-        // If interaction was successful, hide the prompt temporarily
-        if (interactSuccessful)
-        {
-            OnInteractableLost?.Invoke();
-        }
     }
 
     void EndInteraction()
